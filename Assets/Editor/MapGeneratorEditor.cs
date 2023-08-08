43c4014e828483e34c2c538ea7ba7cd43efd519d
Taner Selim <taner.selim@gameforge.com>
2023-08-08 16:54:02 +0200

    Initial commit map generator

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        MapGenerator mapGen = target as MapGenerator;
+
+        if(DrawDefaultInspector())
+        {
+            if(mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
